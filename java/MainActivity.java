cd521b64118420b0b0059988ea5282c7475ed1a8
wdong007 <wdong@iastate.edu>
2017-09-27 15:31:20 -0500

    Add files via upload

---
+++ java/MainActivity.java
@@ -0,0 +1,33 @@
+package com.wdong.mysqldbdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText ET_NAME, ET_PASS;
+    String login_name, login_pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        ET_NAME = (EditText)findViewById(R.id.user_name);
+        ET_PASS = (EditText)findViewById(R.id.user_pass);
+    }
+
+    public void userReg(View view) {
+
+        startActivity(new Intent(this, Register.class));
+    }
+
+    public void userLogin(View view) {
+        login_name = ET_NAME.getText().toString();
+        login_pass = ET_PASS.getText().toString();
+        String method = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, login_name, login_pass);
+    }
+}
